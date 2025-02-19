8d18b95380aa8c9fe9244c88388652ecf7ef4004
Ibrian Danilo Giorgio <ibrian.giorgio@gmail.com>
2025-02-19 08:29:49 -0300

    cambios

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,10 +1,7 @@
+import products2 from '../assets/images/products2.jpg';
+import gobo3 from '../assets/images/gobo3.jpg';
+
 export const data = [
-    {
-      id: 1,
-      imgUrl: "products2.jpg",  // Solo el nombre del archivo
-    },
-    {
-      id: 2,
-      imgUrl: "gobo3.jpg",  // Solo el nombre del archivo
-    },
-  ];
+  { id: 1, imgUrl: products2 },
+  { id: 2, imgUrl: gobo3 },
+];

b6cb69a1a2367f9b9e504c29e51d0c246babae1a
Ibrian Danilo Giorgio <ibrian.giorgio@gmail.com>
2025-02-18 22:35:18 -0300

    fix front home

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,11 +1,8 @@
 export const data = [
     {
-        id:1,imgUrl:"src/assets/images/set01.png"
+        id:1,imgUrl:"src/assets/images/products2.jpg"
     },
     {
-        id:2,imgUrl:"src/assets/images/products2.jpg"
-    },
-    {
-        id:3,imgUrl:"src/assets/images/gobo3.jpg"
+        id:2,imgUrl:"src/assets/images/gobo3.jpg"
     }
 ]
